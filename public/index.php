5a4d3a82747ed52e8c15321462ea37226528f62f
Leonardo Anthony <leonardoanthony.dev@gmail.com>
2023-11-27 11:08:14 -0300

    🔵 #2 Clean Architecture - Domain Layer

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Domain\Entities\Registration;
+use App\Domain\ValueObjects\Cpf;
+use App\Domain\ValueObjects\Email;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$registration = new Registration();
+
+$registration->setName('Leonardo Anthony')
+    ->setBirthDate(new DateTimeImmutable('2000-01-01'))
+    ->setEmail(new Email('leonardoanthony.dev@gmail.com'))
+    ->setRegistrationAt(new DateTimeImmutable())
+    ->setRegistrationNumber(new Cpf('660.210.800-09'));
+;
+
+echo "<pre>";
+print_r($registration);
+echo "</pre>";
---
+++ src/Domain/Entities/Registration.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Entities;
+
+use App\Domain\ValueObjects\Cpf;
+use App\Domain\ValueObjects\Email;
+use DateTimeInterface;
+
+final class Registration
+{
+    private string $name;
+    private Email $email;
+    private DateTimeInterface $birthDate;
+    private Cpf $registrationNumber;
+    private DateTimeInterface $registrationAt;
+
+    /**
+     * @return string;
+     */
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name;
+     */
+    public function setName(string $name): Registration
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return Email;
+     */
+
+     public function getEmail(): Email
+     {
+         return $this->email;
+     }
+ 
+     /**
+      * @param Email $email;
+      */
+     public function setEmail(Email $email): Registration
+     {
+         $this->email = $email;
+         return $this;
+     }
+
+     /**
+     * @return DateTimeInterface;
+     */
+
+    public function getBirthDate(): DateTimeInterface
+    {
+        return $this->birthDate;
+    }
+
+    /**
+     * @param DateTimeInterface $birthDate;
+     */
+    public function setBirthDate(DateTimeInterface $birthDate): Registration
+    {
+        $this->birthDate = $birthDate;
+        return $this;
+    }
+
+    
+     /**
+     * @return Cpf;
+     */
+
+     public function getRegistrationNumber(): Cpf
+     {
+         return $this->registrationNumber;
+     }
+ 
+     /**
+      * @param Cpf $registrationNumber;
+      */
+     public function setRegistrationNumber(Cpf $registrationNumber): Registration
+     {
+         $this->registrationNumber = $registrationNumber;
+         return $this;
+     }
+
+      /**
+     * @return DateTimeInterface;
+     */
+
+    public function getRegistrationAt(): DateTimeInterface
+    {
+        return $this->registrationAt;
+    }
+
+    /**
+     * @param DateTimeInterface $registrationAt;
+     */
+    public function setRegistrationAt(DateTimeInterface $registrationAt): Registration
+    {
+        $this->registrationAt = $registrationAt;
+        return $this;
+    }
+
+
+}
